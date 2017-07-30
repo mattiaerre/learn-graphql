961affc2537fa2c2c024541b19a12ed6fe0928f5
mattia richetto <mattia.richetto@gmail.com>
2017-07-30 11:54:36 -0700

    more types

--- schema/index.js
+++ schema/index.js
@@ -1,40 +1,84 @@
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = require('graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
+} = require('graphql');
 
 let counter = 42;
 
+let counters = [
+  10, 40, 100
+];
+
+const CounterType = new GraphQLObjectType({
+  name: 'CounterType',
+  fields: {
+    value: {
+      type: GraphQLInt,
+      resolve: _ => _ // info: I can avoid this if I like
+    }
+  }
+});
+
+const QueryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve: () => 'world'
+    },
+    answer: {
+      type: GraphQLInt,
+      resolve: () => counter
+    },
+    counters: {
+      type: new GraphQLList(GraphQLInt),
+      resolve: () => counters
+    },
+    counter: {
+      type: CounterType,
+      resolve: () => counter
+    }
+  }
+});
+
+const MutationType = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    incrementAnswer: {
+      type: GraphQLInt,
+      resolve: () => {
+        counter += 1;
+        return counter;
+      }
+    },
+    incrementAnswerBy5: {
+      type: GraphQLInt,
+      resolve: () => {
+        counter += 5;
+        return counter;
+      }
+    },
+    incrementAnswerBy: {
+      type: GraphQLInt,
+      args: {
+        increment: {
+          type: GraphQLInt
+        }
+      },
+      resolve: (_, args) => {
+        counter += args.increment;
+        return counter;
+      }
+    }
+  }
+});
+
 const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'Query',
-    fields: {
-      hello: {
-        type: GraphQLString,
-        resolve: () => 'world'
-      },
-      answer: {
-        type: GraphQLInt,
-        resolve: () => counter
-      }
-    }
-  }),
-  mutation: new GraphQLObjectType({
-    name: 'Mutation',
-    fields: {
-      incrementAnswer: {
-        type: GraphQLInt,
-        resolve: () => {
-          counter += 1;
-          return counter;
-        }
-      },
-      incrementAnswerBy5: {
-        type: GraphQLInt,
-        resolve: () => {
-          counter += 5;
-          return counter;
-        }
-      }
-    }
-  })
+  query: QueryType,
+  mutation: MutationType
 });
 
 module.exports = { schema };
--- server.js
+++ server.js
@@ -1,26 +1,6 @@
 const express = require('express');
-const { graphql } = require('graphql');
 const graphqlHTTP = require('express-graphql');
 const { schema, rootValue } = require('./schema/');
-
-/*
-async function executeSource(source) {
-  const response = await graphql({
-    schema,
-    rootValue,
-    source
-  });
-  return response;
-}
-*/
-
-/*
-app.use('/graphql', async (req, res) => {
-  const source = req.query.source;
-  const data = await executeSource(source);
-  res.send(data);
-});
-*/
 
 const app = express();
 
